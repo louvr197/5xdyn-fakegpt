844c2a645f6dacce08d353e191e07672a049e2a5
louvr197 <134929961+louvr197@users.noreply.github.com>
2025-12-12 14:52:41 +0100

    feat: add routes for conversations and messages
    - conversations resource (index, store, show, update, destroy)
    - conversations/{conversation}/messages POST route
    - users/model PATCH route for model preference

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,19 @@ use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
 use App\Http\Controllers\AskController;
+use App\Http\Controllers\ConversationController;
+use App\Http\Controllers\MessageController;
+use App\Http\Controllers\UserModelController;
 
 Route::get('/ask', [AskController::class, 'index'])->middleware('auth')->name('ask.index');
 Route::post('/ask', [AskController::class, 'ask'])->middleware('auth')->name('ask.post');
+
+// Routes pour le chat avec conversations
+Route::middleware('auth')->group(function () {
+    Route::resource('conversations', ConversationController::class);
+    Route::post('conversations/{conversation}/messages', [MessageController::class, 'store'])->name('conversations.messages.store');
+    Route::patch('users/model', [UserModelController::class, 'update'])->name('users.model.update');
+});
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
